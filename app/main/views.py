3c15689a2fad1cc6221ccf6a014252a40650d7de
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-09-16 14:56:51 +0300

    view function for new subsr=cribe thanks page desined

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,7 @@ from . import main
 from ..models import User,Comments, Blog, Mailer
 from .. import db,photos
 from .forms import CommentForm, SubscribeForm
-from flask_login import login_required,current_user
+from flask_login import login_required,logout_user,current_user
 import markdown2
 
 @main.route('/home')
@@ -11,6 +11,8 @@ def home():
     '''
     View function to render the index html template, that directs users to select their log in
     '''
+
+    message = 'Thank You for subscribing. Stay tuned for massive content. Watch this space!'
 
     return render_template('home.html')
 
@@ -43,8 +45,9 @@ def new_comments(id):
     if comment_form.validate_on_submit():
         email = Mailer.query.filter_by(emails = comment_form.email.data).first()
         if email is not None:
-            new_comment = Comments(comment=comment_form.comment.data, blog_comment=id,)
-            new_comment.save_comment()
+            new_comment = Comments(comment=comment_form.comment.data, blog_comment=id)
+            db.session.add(new_comment)
+            db.session.commit()
 
             return redirect(url_for('main.index'))
     title = 'What do you think about this Post? '
@@ -66,7 +69,7 @@ def index():
         new_subscriber = Mailer(name= subscription_form.comment.data, emails = subscription_form.email.data)
         new_subscriber.save_mail()
 
-        return redirect(url_for('main.index'))
+        return redirect(url_for('main.home'))
 
 
 
